b653d1cb87dba3a8c9ccf2646007209ee82eae8f
stephcmartin <martin.stephaniechow@gmail.com>
2018-01-12 19:09:28 -0500

    Added new const for api key

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+
+const api_key = process.env.API_KEY;
 
 // Create a new component. This should produce some HTMl
 
 const App = () => {
-    return <div>Hello!</div>;
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    )
 }
 
 // Take generated componented and place it on the DOM
